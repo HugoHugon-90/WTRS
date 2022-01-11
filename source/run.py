cfec6c9c37106c77114f6faf113202cfbfc9837b
Hugo Pereira Hugon <hugo.hugon@tecnico.ulisboa.pt>
2022-01-11 21:56:44 +0000

    second commit test

--- source/run.py
+++ source/run.py
@@ -1,7 +1,11 @@
 from helpers import constants as c
+import sympy as sym
+
+x = sym.symbols('x')
 
 def run():
     print(c.SAY_HELLO)
+    print(sym.diff(x**3))
 
 if __name__ == '__main__':
     run()
