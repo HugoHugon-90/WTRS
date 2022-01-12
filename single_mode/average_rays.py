7475baefc9226a77d1b420da0eff1f1841ee4796
Hugo Pereira Hugon <hugo.hugon@tecnico.ulisboa.pt>
2022-01-12 02:51:51 +0000

    fixed last

--- single_mode/average_rays.py
+++ single_mode/average_rays.py
@@ -1,6 +1,8 @@
 #2-D evolution eqs for the average rays
+from cmath import sqrt
 
-class Average_Ray_Equations:
+
+class Average_Rays:
 
     def __init__(self, x0, y0, kx0, ky0, dx0, dy0, dkx0, dky0, dxdx0, dxdy0, dydy0, dkxdkx0, dkxdky0, dkydky0, dxdne, dydne, dkxdne, dkydne, dxdnedr, dydnedr, dnedne, t):
 
@@ -30,4 +32,27 @@ class Average_Ray_Equations:
 
 
 
+    def dxdt(self, w):
 
+        x, y, kx, ky, dx, dy, dkx, dky,  dxdx, dxdy, dydy, dkxdkx, dkxdky, dkydky, dxdne, dydne, dkxdne, dkydne, dxdnedr, dydnedr, dnedne = w
+
+        k = sqrt(kx ** 2 + ky ** 2)
+        k3 = k ** 3
+        k5 = k ** 5
+        ky2 = ky ** 2
+        ky3 = ky ** 3
+        kx2 = kx ** 2
+        kx3 = kx ** 3
+
+        #Eq. A1 with <ne> = 1
+        dxdt = kx / k \
+               + (3/2) * ( kx * ky2 * dkydky + 2* kx * ky * dkxdky + kx3 * dkxdkx ) / k5 \
+               - ( (3/2)*( kx * dkxdkx ) + (1/2) * ( kx * dkydky + 2 * ky * dkxdky ) ) / k3 \
+               - dkxdne / k \
+               + (kx2 * dkxdne + kx * ky * dkydne) / k3 \
+               - (kx / k) (dxdnedr + dydnedr) \
+               - kx * dnedne / k
+
+        return dxdt
+
+        #TODO: Finish writing all the partial equations
--- single_mode/dsolver.py
+++ single_mode/dsolver.py
@@ -1,7 +1,4 @@
-from cmath import sqrt
-
-from single_mode.average_rays import Average_Ray_Equations
-
+from single_mode.average_rays import Average_Rays
 from scipy.integrate import odeint
 
 
@@ -11,34 +8,18 @@ from scipy.integrate import odeint
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.odeint.html
 
 
-class Dsolver(Average_Ray_Equations):
+class Dsolver(Average_Rays):
 
-    def vectorfield(w, t, p):
+    def vectorfield(self, w, t, p):
 
         """
         Arguments:
             w :  vector of the state variables
             t :  time
-            p :  vector of the parameters
-
         """
         x, y, kx, ky, dx, dy, dkx, dky,  dxdx, dxdy, dydy, dkxdkx, dkxdky, dkydky, dxdne, dydne, dkxdne, dkydne, dxdnedr, dydnedr, dnedne = w
 
-        k = sqrt(kx ** 2 + ky ** 2)
-        k3 = k ** 3
-        k5 = k ** 5
-        ky2 = ky ** 2
-        ky3 = ky ** 3
-        kx2 = kx ** 2
-        kx3 = kx ** 3
-
-        #Eq. A1 with <ne> = 1
-        dxdt = kx / k + (3/2) * ( kx * ky2 * dkydky + 2* kx * ky * dkxdky + kx3 * dkxdkx ) / k5
-        - ( (3/2)( kx * dkxdkx ) + (1/2) * ( kx * dkydky + 2 * ky * dkxdky ) ) / k3
-        - dkxdne / k + (kx2 * dkxdne + kx * ky * dkydne) / k3
-        - (kx / k) (dxdnedr + dydnedr) - kx * dnedne / k
-
-        #TODO: Finish writing all the partial equations
+        dxdt = self.dxdt(w)
 
         dfdt = [dxdt]
         return dfdt
