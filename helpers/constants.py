434654ae87e52b01e16fc6dad44a7c17e05dda09
Hugo Pereira Hugon <hugo.hugon@tecnico.ulisboa.pt>
2022-01-13 02:16:24 +0000

    input parameters clean-up

--- helpers/constants.py
+++ helpers/constants.py
@@ -4,3 +4,34 @@ from cmath import sqrt
 k0_obl = [1. / abs(sqrt(2)), 1. / abs(sqrt(2))]
 k0_par = [1., 0.]
 k0_perp = [0., 1.]
+
+# initial position (please don't change)
+r0 = [0, 0]
+
+# initial first-order fluctuations
+dx0 = 0.
+dy0 = 0.
+dkx0 = 0.
+dky0 = 0.
+
+# initial second-order fluctuations
+dxdx0 = 0.
+dydy0 = 0.
+dxdy0 = 0.
+dkxdkx0 = 0.
+dkydky0 = 0.
+dkxdky0 = 0.
+dxdkx0 = 0.
+dxdky0 = 0.
+dydkx0 = 0.
+dydky0 = 0.
+
+# initial density-dependent fluctuations
+dxdne = 0.
+dydne = 0.
+dkxdne = 0.
+dkydne = 0.
+dxdnedx = 0.
+dydnedx = 0.
+dkxdnedx = 0.
+dkydnedx = 0.
--- single_mode/input_parameters.py
+++ single_mode/input_parameters.py
@@ -9,40 +9,8 @@ k0 = [abs(cos(pi/3.)), abs(sin(pi/3.))]
 # turbulence amplitude as a percentage; [0,1], << 1
 dne0 = 0.04
 
-# turbulence wavenumber normalized to the unperturbed wavevector k_0; [0,1], <<1
+# turbulence wavenumber normalized to the unperturbed wavevector k_0; [0,1], << 1
 q0 = 0.04
-
-# initial position (please don't change)
-r0 = [0, 0]
-
-# initial first-order fluctuations
-dx0 = 0.
-dy0 = 0.
-dkx0 = 0.
-dky0 = 0.
-
-# initial second-order fluctuations
-dxdx0 = 0.
-dydy0 = 0.
-dxdy0 = 0.
-dkxdkx0 = 0.
-dkydky0 = 0.
-dkxdky0 = 0.
-dxdkx0 = 0.
-dxdky0 = 0.
-dydkx0 = 0.
-dydky0 = 0.
-
-# initial density-dependent fluctuations
-dxdne = 0.
-dydne = 0.
-dkxdne = 0.
-dkydne = 0.
-dxdnedx = 0.
-dydnedx = 0.
-dkxdnedx = 0.
-dkydnedx = 0.
-
 
 # integrator configs
 num_points = 10000
